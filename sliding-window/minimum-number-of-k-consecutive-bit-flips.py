25abfb7f31cf3275d0c545403362457e16def403
nhp93 <154722749+nhp93@users.noreply.github.com>
2026-06-25 22:18:49 +0700

    :ballot_box_with_check: minimum-number-of-k-consecutive-bit-flips [Runtime: 74 ms; Memory: 25.2 MB]

--- sliding-window/minimum-number-of-k-consecutive-bit-flips.py
+++ sliding-window/minimum-number-of-k-consecutive-bit-flips.py
@@ -11,18 +11,8 @@ class Solution:
             if nums[i] == 0 and len(dq) % 2 == 0 or nums[i] == 1 and len(dq) % 2 == 1:
                 if  i + k > len(nums):
                     return -1
-                    
+
                 dq.append(i+k)
                 flips += 1
             
-        return flips
-
-                # if i + k > n:
-                #     return -1
-                # for m in range(k):
-                #     if nums[i + m] == 1:
-                #         nums[i + m] = 0
-                #     else:
-                #         nums[i + m] = 1
-                # flips += 1
         return flips
