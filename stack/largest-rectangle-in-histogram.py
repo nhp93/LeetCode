ee38751ace6a87992bf6fa86833b340d3746521b
nhp93 <154722749+nhp93@users.noreply.github.com>
2026-06-27 21:10:03 +0700

    :ballot_box_with_check: largest-rectangle-in-histogram [Runtime: N/A; Memory: N/A]

--- stack/largest-rectangle-in-histogram.py
+++ stack/largest-rectangle-in-histogram.py
@@ -10,9 +10,6 @@ class Solution:
                 if heights[i] >= heights[stack[-1]]:
                     stack.append(i)
                 else:
-                    # area = (i - stack[-1] + 1) * heights[stack[-1]]
-                    # if area > max_area:
-                    #     max_area = area
                     left = 0
                     right = stack[-1]
                     while stack and heights[stack[-1]] > heights[i]:
@@ -21,7 +18,11 @@ class Solution:
                     if area > max_area:
                         max_area = area
                     stack.append(i)
-        area = (n - 1 - stack[-1] + 1) * heights[stack[-1]]
-        if area > max_area:
-            max_area = area
+        while stack:
+            h = heights[stack.pop()]
+            if not stack:
+                w = n
+            else:
+                w = n - stack[-1] - 1
+            max_area = max(max_area, h * w)
         return max_area
