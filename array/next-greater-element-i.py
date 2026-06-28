edc30a6a876e47da4a7ba172ed7ad1eb7e661572
nhp93 <154722749+nhp93@users.noreply.github.com>
2026-06-28 21:17:11 +0700

    :ballot_box_with_check: next-greater-element-i [Runtime: 0 ms; Memory: 19.5 MB]

--- array/next-greater-element-i.py
+++ array/next-greater-element-i.py
@@ -2,17 +2,15 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack = []
         res = []
-        hash_map = defaultdict(int)
-        for i in range(len(nums2) - 1):
-            num1 = nums2[i]
-            num2 = nums2[i+1]
-            stack.append(num1)
-            if num1 < num2:
-                while stack:
-                    hash_map[stack.pop()] = num2
+        hash_map = {}
+        for i in range(len(nums2)):
+            num2 = nums2[i]
+            while stack and num2 > stack[-1]:
+                hash_map[stack.pop()] = num2
+            stack.append(num2)
         
         for num in nums1:
-            if hash_map[num] > 0:
+            if num in hash_map:
                 res.append(hash_map[num])
             else:
                 res.append(-1)
