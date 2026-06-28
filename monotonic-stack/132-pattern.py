2f2961a67cef63692913b13f9ff5038079cb17bb
nhp93 <154722749+nhp93@users.noreply.github.com>
2026-06-28 17:42:56 +0700

    :ballot_box_with_check: 132-pattern [Runtime: N/A; Memory: N/A]

---
+++ monotonic-stack/132-pattern.py
@@ -0,0 +1,22 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        left = float('-inf')
+        stack = []
+        for num in nums:
+            if not stack:
+                stack.append(num)
+                left = num
+            elif len(stack) == 1:
+                if num > stack[-1]:
+                    stack.append(num)
+                elif num < stack[-1]:
+                    stack.pop()
+                    stack.append(num)
+                    left = num
+            elif len(stack) == 2:
+                if num < stack[-1] and num > left:
+                    return True
+                if num > stack[-1]:
+                    stack.pop()
+                    stack.append(num)
+        return False
