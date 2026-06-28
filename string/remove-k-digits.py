b6f9a654cf39a7119b4780f7d84d5905d0fce18b
nhp93 <154722749+nhp93@users.noreply.github.com>
2026-06-28 17:37:35 +0700

    :ballot_box_with_check: remove-k-digits [Runtime: N/A; Memory: N/A]

--- string/remove-k-digits.py
+++ string/remove-k-digits.py
@@ -2,22 +2,13 @@ class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
         stack = []
         n = len(num)
-        if k == n:
+        if k >= n:
             return "0"
         for i in range(n):
-            if not stack:
-                stack.append(num[i])
-            else:
-                if k > 0:
-                    if num[i] < stack[-1]:
-                        stack.pop()
-                        stack.append(num[i])
-                        k -= 1
-                    else:
-                        stack.append(num[i])
-                else:
-                    stack.append(num[i])
+            while k > 0 and stack and num[i] < stack[-1]:
+                stack.pop()
+                k -= 1
+            stack.append(num[i])
         if k > 0:
             stack = stack[:-k]
-        res = "".join(stack)
-        return str(int(res))
+        return "".join(stack).lstrip("0")
