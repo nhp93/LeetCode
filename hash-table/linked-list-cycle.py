40d939505bd2dfe3d5b6d0be87d2a1fbbe5d7558
nhp93 <154722749+nhp93@users.noreply.github.com>
2026-06-29 22:48:42 +0700

    :ballot_box_with_check: linked-list-cycle [Runtime: 48 ms; Memory: 22.4 MB]

--- hash-table/linked-list-cycle.py
+++ hash-table/linked-list-cycle.py
@@ -1,21 +1,17 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def hasCycle(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
-        node_seen = set()
-        current = head
-        while current is not None:
-            if current in node_seen:
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow_pointer = head
+        fast_pointer = head
+        while fast_pointer and fast_pointer.next:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+            if slow_pointer == fast_pointer:
                 return True
-            node_seen.add(current)
-            current = current.next
+            
         return False
-        
