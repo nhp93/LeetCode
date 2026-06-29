7b61014ba0b5093fa5a1aab608cfda85cd0a288a
nhp93 <154722749+nhp93@users.noreply.github.com>
2026-06-29 18:22:58 +0700

    :ballot_box_with_check: longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit [Runtime: N/A; Memory: N/A]

--- array/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ array/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,16 +1,26 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        dq = deque()
+        min_dq = deque()
+        max_dq = deque()
         max_leng = float('-inf')
+        l = 0
         if len(nums) == 1:
             return 1
-        for num in nums:
-            if not dq:
-                dq.append(num)
-            else:
-                while dq and abs(dq[0] - num) > limit:
-                    dq.popleft()
-                dq.append(num)
-                max_leng = max(len(dq), max_leng)
+        for r in range(len(nums)):
+            while min_dq and nums[min_dq[-1]] >= nums[r]:
+                min_dq.pop()
+            while max_dq and nums[max_dq[-1]] <= nums[r]:
+                max_dq.pop()
 
+            min_dq.append(r)
+            max_dq.append(r)
+            
+            while abs(nums[min_dq[0]] - nums[max_dq[0]]) > limit:
+                l += 1
+                if min_dq[0] < l:
+                    min_dq.popleft()
+                if max_dq[0] < l:
+                    max_dq.popleft()
+
+            max_leng = max(max_leng, r - l + 1)
         return max_leng
