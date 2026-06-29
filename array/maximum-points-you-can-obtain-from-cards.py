aa7b8da0516f91bed484ecd6926d4610fe4b48eb
nhp93 <154722749+nhp93@users.noreply.github.com>
2026-06-29 22:31:55 +0700

    :ballot_box_with_check: maximum-points-you-can-obtain-from-cards [Runtime: 38 ms; Memory: 32.2 MB]

--- array/maximum-points-you-can-obtain-from-cards.py
+++ array/maximum-points-you-can-obtain-from-cards.py
@@ -2,21 +2,15 @@ class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         prefix_sum = list(accumulate(cardPoints))
         suffix_sum = list(accumulate(cardPoints[::-1]))
-        print(prefix_sum)
-        print(suffix_sum)
         max_sum = float('-inf')
         for i in range(k+1):
             if i == 0:
                 total = prefix_sum[k-1]
-                print(total)
                 max_sum = max(max_sum, total)
             elif i == k:
                 total = suffix_sum[k-1]
                 max_sum = max(max_sum, total)
             else:
                 total = prefix_sum[k-i-1] + suffix_sum[i-1]
-                print(total)
-                print("I")
-                print(i)
                 max_sum = max(max_sum, total)
         return max_sum
