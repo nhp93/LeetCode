11cf42a9c7151ee78412ccbbea22c258dc5b797f
nhp93 <154722749+nhp93@users.noreply.github.com>
2026-07-02 09:08:26 +0700

    :ballot_box_with_check: plus-one-linked-list [Runtime: 42 ms; Memory: 19.2 MB]

--- linked-list/plus-one-linked-list.py
+++ linked-list/plus-one-linked-list.py
@@ -7,21 +7,37 @@ class Solution:
     def plusOne(self, head: ListNode) -> ListNode:
         prev = None
         curr = head
+        count = 0
         while curr:
             next_node = curr.next
             curr.next = prev
             prev = curr
             curr = next_node
+            count += 1
         
         curr = prev
-        while curr.val == 9:
-            if curr.next == None:
-                curr.next = ListNode(1)
-                curr.val = 0
+        counts = 0
+        while curr:
+        #     if curr.next == None:
+        #         curr.next = ListNode(1)
+        #         curr.val = 0
+        #     else:
+        #         curr.val = 0
+        #         curr = curr.next
+        #         counts += 1
+        # if count != counts:
+        #     curr.val += 1
+            if curr.val == 9:
+                if curr.next == None:
+                    curr.next = ListNode(1)
+                    curr.val = 0
+                    break
+                else:
+                    curr.val = 0
+                    curr = curr.next
             else:
-                curr.val = 0
-                curr = curr.next
-        curr.val += 1
+                curr.val += 1
+                break
         
         head = prev
         prev = None
