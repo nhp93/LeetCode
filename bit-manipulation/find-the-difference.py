7a8f1c5f577e1f551cd5faacf3b1fcec58fb7631
nhp93 <154722749+nhp93@users.noreply.github.com>
2026-07-17 06:46:49 +0700

    :ballot_box_with_check: find-the-difference [Runtime: 0 ms; Memory: 19.4 MB]

--- bit-manipulation/find-the-difference.py
+++ bit-manipulation/find-the-difference.py
@@ -1,7 +1,40 @@
+'''
+Input: 2 strings
+Output: character that is the difference btw 2 strings
+Edge Cases: len(t) <= len(s): 
+len(s) == 0: return t
+
+Planning:
+Using Counter for both string s and t
+Return the different between 2 dictionaries
+
+counter_t = {
+    "a": 1
+    "b": 1
+    "c": 1
+    "d": 1
+    "e": 1
+}
+abcdeabcd
+counter_s = {
+    "a": 1
+    "b": 1
+    "c": 1
+    "d": 1
+}
+adbcacbd
+'''
 class Solution:
+    from collections import Counter
     def findTheDifference(self, s: str, t: str) -> str:
-        s_counter = Counter(s)
-        t_counter = Counter(t)
-        for key in t_counter:
-            if t_counter[key] > s_counter[key]:
+        if len(t) <= len(s):
+            return
+        counter_s = Counter(s)
+        counter_t = Counter(t)
+        for key in counter_t:
+            if counter_t[key] > counter_s[key]:
                 return key
+    
+        
+
+        
