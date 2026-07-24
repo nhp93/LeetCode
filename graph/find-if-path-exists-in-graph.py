a17867fd17a88e71da99ebe4c9af4feb9114cbf7
nhp93 <154722749+nhp93@users.noreply.github.com>
2026-07-24 23:52:55 +0700

    :ballot_box_with_check: find-if-path-exists-in-graph [Runtime: N/A; Memory: N/A]

---
+++ graph/find-if-path-exists-in-graph.py
@@ -0,0 +1,22 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = defaultdict(list)
+
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        queue = deque([source])
+        visited = set()
+        visited.add(source)
+
+        while queue:
+            curr = queue.popleft()
+            if curr == destination:
+                return True
+            
+            for neighbor in graph[curr]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+        return False
