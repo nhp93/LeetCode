3e88687cb2fecf81bf1aa8ad1d7e716cd8bf8c04
nhp93 <154722749+nhp93@users.noreply.github.com>
2026-08-03 22:25:27 +0700

    :ballot_box_with_check: same-tree [Runtime: 0 ms; Memory: 19.4 MB]

---
+++ breadth-first-search/same-tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def dfs(tree1, tree2):
+            if tree1 is None and tree2 is None:
+                return True
+            if tree1 is None or tree2 is None:
+                return False
+            if tree1.val != tree2.val:
+                return False
+            left_same = dfs(tree1.left, tree2.left)
+            right_same = dfs(tree1.right, tree2.right)
+            return left_same and right_same
+        return dfs(p, q)
